552d845124a2e599b43ab6ea6eb45d4ee32618a7
Yumin <elin0225@naver.com>
2020-07-15 05:07:31 +0900

    remove basic

--- basic_100/1008.py
+++
@@ -1,5 +0,0 @@
-import io, sys
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
-print("\u250C\u252C\u2510")
-print("\u251C\u253C\u2524")
-print("\u2514\u2534\u2518")
--- basic_100/1015.py
+++
@@ -1,2 +0,0 @@
-a = float(input())
-print("%.2f" %a)

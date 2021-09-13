3d5e05e96c96fceb0fe66634421fd4094fcfa268
yumin <49578987+yumin25@users.noreply.github.com>
2021-09-13 22:43:12 +0900

    21.09.13

---
+++ boj/백트래킹/15649 N과 M(1).java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	static int[] answer;
+	static boolean[] selected;
+	static int N, M;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		answer = new int[M];
+		selected = new boolean[N+1];
+		sb = new StringBuilder();
+		Permutation(0);
+		System.out.println(sb);
+	}
+	
+	public static void Permutation(int count) {
+		if(count==M) {
+			for(int i:answer) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=1;i<=N;i++) {
+			if(selected[i]) continue;
+			selected[i] = true;
+			answer[count] = i;
+			Permutation(count+1);
+			selected[i] = false;
+		}
+	}
+}
